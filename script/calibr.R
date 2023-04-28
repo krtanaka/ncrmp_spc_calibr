4a840f28457fa4ae7a76b7f18257c3ac7cdb1e7b
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-04-28 10:51:37 -1000

    ok

--- script/calibr.R
+++ script/calibr.R
@@ -6,61 +6,81 @@ library(ggplot2)
 
 rm(list = ls())
 
-var = c("abund", "biom")[1]
+var = c("abund", "biom")
+region = c("MHI", "MARIAN", "NHWI", "PRIAs", "SAMOA")
+model = c("GLM", "GLMM")
 
-belt <- readRDS(paste0("G:/ncrmp_spc/belt.site.", var, ".20002009.rds")) %>% 
-  group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
-  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
-  na.omit() %>% 
-  mutate(PRESENCE = as.integer(DENSITY > 0),
-         BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
-         GROUP = SPECIES) %>% 
-  select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
-
-spc = readRDS(paste0("data/nSPC.site.", var, ".size.20092022.MHI.rds")) %>% 
-  group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
-  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
-  na.omit() %>% 
-  mutate(PRESENCE = as.integer(DENSITY > 0),
-         BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
-         GROUP = SPECIES) %>% 
-  select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
-
-tow = readRDS(paste0("data/tow.segment.", var, ".size.20002017.MHI.rds")) %>% 
-  subset(CENTROIDLON < 0) %>%
-  mutate(DEPTH_BIN = case_when(
-    DEPTH >= 0  & DEPTH <= 6 ~ "Shallow",
-    DEPTH > 6  & DEPTH <= 18 ~ "Mid",
-    DEPTH > 18 ~ "Deep",
-    TRUE ~ ""),
-    LONGITUDE = CENTROIDLON,
-    LATITUDE = CENTROIDLAT) %>% 
-  group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
-  summarise(DENSITY = sum(!!sym(paste0(var, ".segment"))), .groups = "drop") %>% 
-  na.omit() %>% 
-  mutate(PRESENCE = as.integer(DENSITY > 0),
-         BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
-         GROUP = SPECIES)  %>% 
-  select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
-
-calibr = c("spc_belt", "spc_tow")
-
-for (c in 1:length(calibr)) {
+spc_calibr = function(var, region, model){
   
-  # c = 2
+  # var = "abund"
+  # region = "MHI"
+  # model = "GLM"
   
-  if (calibr[c] == "spc_belt") set = rbind(spc, belt)
-  if (calibr[c] == "spc_tow") set = rbind(spc, tow)
-
-  set$REP <- sprintf("%04d", as.numeric(factor(paste(set$LATITUDE, set$LONGITUDE, set$DATE_, sep="_"))))
-  set$REP = as.integer(set$REP)
-  set = set[,c("BLOCK", "REP", "GROUP", "METHOD", "DENSITY", "PRESENCE")]
-  set[] <- lapply(set, trimws)
-  set$DENSITY = as.numeric(set$DENSITY)
-  set$PRESENCE = as.numeric(set$PRESENCE)
-  results <- run_calibr(set, std_method = "nSPC", stat_model = "GLMM", min_obs = 10)
-  export_results(results, outdir = paste0("output/", calibr[c], "_", var))
+  belt <- readRDS(paste0("data/belt.site.", var, ".size.20002009.", region, ".rds")) %>% 
+    group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+    summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
+    na.omit() %>% 
+    mutate(PRESENCE = as.integer(DENSITY > 0),
+           BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
+           GROUP = SPECIES) %>% 
+    select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
+  
+  spc = readRDS(paste0("data/nSPC.site.", var, ".size.20092022.", region, ".rds")) %>% 
+    group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+    summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
+    na.omit() %>% 
+    mutate(PRESENCE = as.integer(DENSITY > 0),
+           BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
+           GROUP = SPECIES) %>% 
+    select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
+  
+  tow = readRDS(paste0("data/tow.segment.", var, ".size.20002017.", region, ".rds")) %>% 
+    subset(CENTROIDLON < 0) %>%
+    mutate(DEPTH_BIN = case_when(
+      DEPTH >= 0  & DEPTH <= 6 ~ "Shallow",
+      DEPTH > 6  & DEPTH <= 18 ~ "Mid",
+      DEPTH > 18 ~ "Deep",
+      TRUE ~ ""),
+      LONGITUDE = CENTROIDLON,
+      LATITUDE = CENTROIDLAT) %>% 
+    group_by(ISLAND, DEPTH_BIN, REEF_ZONE, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+    summarise(DENSITY = sum(!!sym(paste0(var, ".segment"))), .groups = "drop") %>% 
+    na.omit() %>% 
+    mutate(PRESENCE = as.integer(DENSITY > 0),
+           BLOCK = paste(ISLAND, DEPTH_BIN, REEF_ZONE, sep = "."),
+           GROUP = SPECIES)  %>% 
+    select(DATE_, LATITUDE, LONGITUDE, BLOCK, GROUP, METHOD, DENSITY, PRESENCE)
+  
+  calibr = c("spc_belt", "spc_tow")
+  
+  for (c in 1:length(calibr)) {
+    
+    # c = 2
+    
+    if (calibr[c] == "spc_belt") set = rbind(spc, belt)
+    if (calibr[c] == "spc_tow") set = rbind(spc, tow)
+    
+    set$REP <- sprintf("%04d", as.numeric(factor(paste(set$LATITUDE, set$LONGITUDE, set$DATE_, sep="_"))))
+    set$REP = as.integer(set$REP)
+    set = set[,c("BLOCK", "REP", "GROUP", "METHOD", "DENSITY", "PRESENCE")]
+    set[] <- lapply(set, trimws)
+    set$DENSITY = as.numeric(set$DENSITY)
+    set$PRESENCE = as.numeric(set$PRESENCE)
+    results <- run_calibr(set, std_method = "nSPC", stat_model = model)
+    if(!file.exists(paste0("output/", calibr[c], "_", var, "_", region, "_", model))) dir.create(paste0("output/", calibr[c], "_", var, "_", region, "_", model))
+    export_results(results, outdir = paste0("output/", calibr[c], "_", var, "_", region, "_", model))
+    
+  }
+  
   
 }
 
+spc_calibr("abund", "MHI", "GLM")
+spc_calibr("abund", "MHI", "GLMM")
+spc_calibr("biom", "MHI", "GLM")
+spc_calibr("biom", "MHI", "GLMM")
 
+spc_calibr("abund", "SAMOA", "GLM")
+spc_calibr("abund", "SAMOA", "GLMM")
+spc_calibr("biom", "SAMOA", "GLM")
+spc_calibr("biom", "SAMOA", "GLMM")
