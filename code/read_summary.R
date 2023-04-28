747fab2079469292b5acd27c8e6d4833edbc5d5d
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-04-27 16:42:49 -1000

    Update read_summary.R

--- code/read_summary.R
+++ code/read_summary.R
@@ -37,26 +37,61 @@ calibr_belt = read_csv(paste0("output/spc_belt_", var, "/summary_table.csv")) %>
 calibr_tow = read_csv(paste0("output/spc_tow_", var, "/summary_table.csv")) %>% subset(GROUP == "APVI"); calibr_tow = calibr_tow$POS.GCF
 
 belt <- readRDS(paste0("data/belt.site.", var, ".size.20002009.MHI.rds")) %>% 
-  subset(SPECIES == "APVI") %>% 
-  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE) %>% 
-  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop")  %>% 
+  subset(SPECIES == "APVI") %>%
   mutate(DENSITY = (DENSITY / calibr_belt[1]))
 
 spc = readRDS(paste0("data/nSPC.site.", var, ".size.20092022.MHI.rds")) %>% 
-  subset(SPECIES == "APVI") %>% 
-  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE) %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
   summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
-  mutate(DENSITY = DENSITY)
+  subset(SPECIES == "APVI")
 
 tow = readRDS(paste0("data/tow.segment.", var, ".size.20002017.MHI.rds")) %>% 
+  subset(CENTROIDLON < 0) %>%
   mutate(LONGITUDE = CENTROIDLON,
          LATITUDE = CENTROIDLAT) %>% 
-  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE) %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
   summarise(DENSITY = sum(!!sym(paste0(var, ".segment"))), .groups = "drop") %>% 
+  subset(SPECIES == "APVI") %>%
   mutate(DENSITY = (DENSITY / calibr_tow[1]))
 
 df = rbind(spc, belt, tow)
 
+library(lubridate)
+df$YEAR <- year(df$DATE_)
+df$MONTH <- month(df$DATE_)
+
+library(colorRamps)
+
 df %>% 
-  ggplot(aes(LONGITUDE, LATITUDE, size = DENSITY)) + 
-  geom_point()
+  mutate(DENSITY = DENSITY*100) %>% 
+  ggplot(aes(LONGITUDE, LATITUDE)) + 
+  geom_point(aes(size = DENSITY, fill = DENSITY, color = DENSITY), shape = 21, alpha = 0.7) +
+  scale_color_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  scale_fill_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  facet_grid(METHOD~YEAR) +
+  labs(x = expression(paste("Longitude ", degree, "W", sep = "")),
+       y = expression(paste("Latitude ", degree, "N", sep = ""))) +
+  guides(color = guide_legend(expression("Individuals per 100" ~ m^2~"")), 
+         fill = guide_legend(expression("Individuals per 100" ~ m^2~"")),
+         size = guide_legend(expression("Individuals per 100" ~ m^2~""))) + 
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20")) 
+
+df %>% 
+  mutate(DENSITY = DENSITY*100) %>% 
+  # group_by(DEPTH, METHOD) %>% 
+  # summarise(DENSITY = mean(DENSITY)) %>% 
+  ggplot(aes(DEPTH, DENSITY)) + 
+  geom_point(aes(size = DENSITY, fill = DENSITY, color = DENSITY), shape = 21, alpha = 0.7) +
+  scale_color_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  scale_fill_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  facet_grid(~METHOD) +
+  guides(color = guide_legend(expression("Individuals per 100" ~ m^2~"")), 
+         fill = guide_legend(expression("Individuals per 100" ~ m^2~"")),
+         size = guide_legend(expression("Individuals per 100" ~ m^2~""))) + 
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20")) 
