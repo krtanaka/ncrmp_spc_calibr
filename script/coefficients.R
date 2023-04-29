154a0d955197690e4649412c4c0ce74a74575020
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-04-28 15:12:53 -1000

    ok

---
+++ script/coefficients.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+rm(list = ls())
+
+library(readr)
+
+rm(list = ls())
+
+# METHOD: Sampling method used.
+# GCF.PRES: Gear calibration factor for presence-absence data.
+# Convert probability of observation of secondary method following this equation: 
+# Prob_M1=inv.logit(logit(Prob_M2-GCF.PRES)))
+# GCF.PRES_2.5: Lower bound of 95% probability interval of GCF.PRES.
+# GCF.PRES_95: Upper bound of 95% probability interval of GCF.PRES.
+# GCF.POS: Gear calibration factor for positive-only data. Convert abundance metric of positive-only data for secondary method following this equation: 
+# Abund_M1=Abund_M2/GCF.POS
+# GCF.POS_2.5: Lower bound of 95% probability interval of GCF.PRES.
+# GCF.POS_95: Upper bound of 95% probability interval of GCF.PRES.
+# PRES: Probability of observing a specific species for each method in the provided dataset.
+# PRES.CAL: Calibrated probability of observing a species by method (used as a check on the success of the standardization procedure).
+# POS: Abundance of a specific species for each method.
+# POS.CAL: Calibrated abundance of a specific species for each method (used as a check on the success of the standardization procedure).
+# OPUE: Obervation Per Unit Effort obtained by multiplying prob. of sighting and abundance (PRES x POS) in provided data set.
+# OPUE.CAL: Obervation Per Unit Effort obtained by multiplying calibrated prob. of sighting and abundance (PRES.CAL x POS.CAL) in provided data set.
+
+folders <- list.dirs(path = "output", full.names = TRUE, recursive = FALSE)
+
+read_summary_table <- function(folder_path) {
+  file_path <- file.path(folder_path, "summary_table.csv")
+  if (file.exists(file_path)) {
+    df <- read.csv(file_path)
+    return(df)
+  } else {
+    message(paste("File not found:", file_path))
+    return(NULL)
+  }
+}
+
+df_list <- lapply(folders, read_summary_table)
+names(df_list) <- basename(folders)
+
+df <- bind_rows(df_list, .id = "folder") 
+
+df <- df %>%
+  separate(folder, into = c("spc", "belt_tow", "var", "region", "model"), sep = "_")
+
+colnames(df) <- tolower(colnames(df))
+
+df %>% 
+  subset(group == "APVI") %>%
+  subset(model == "GLM") %>% 
+  subset(method != "1_nSPC") %>% 
+  ggplot(aes(region, gcf.pos, color = var)) +
+  geom_point(position=position_dodge(width=0.5), size = 4) +
+  geom_errorbar(aes(ymin = gcf.pos_2.5, ymax = gcf.pos_95), position=position_dodge(width=0.5), width = 0) +
+  facet_wrap(~belt_tow, scales = "free")
+
+df %>% 
+  subset(group == "APVI") %>%
+  group_by(model, region) %>% 
+  summarise(pres.cal = mean(pres.cal, na.rm = T)) %>% 
+  ggplot(aes(region, pres.cal, color = model)) +
+  geom_point(position=position_dodge(width=0.5), size = 4) +
+  facet_wrap(~region, scales = "free")
+
+
---
+++ script/plot.R
@@ -0,0 +1,114 @@
+library(readr)
+
+rm(list = ls())
+
+region = c("MHI", "MARIAN", "NWHI", "PRIAs", "SAMOA")[3]
+var = c("abund", "biom")[1]
+
+calibr_belt = read_csv(paste0("output/spc_belt_", var, "_", region, "_GLMM/summary_table.csv")) %>% 
+  subset(GROUP == "APVI") %>% 
+  subset(METHOD != "1_nSPC")
+
+calibr_tow = read_csv(paste0("output/spc_tow_", var, "_", region, "_GLMM/summary_table.csv")) %>% 
+  subset(GROUP == "APVI") %>% 
+  subset(METHOD != "1_nSPC")
+
+calibr_tow = calibr_tow$POS.GCF
+calibr_belt = calibr_belt$POS.GCF
+
+belt <- readRDS(paste0("data/belt.site.", var, ".size.20002009.", region, ".rds")) %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop")  %>% 
+  subset(SPECIES == "APVI") %>%
+  mutate(DENSITY = (DENSITY / calibr_belt))
+
+spc = readRDS(paste0("data/nSPC.site.", var, ".size.20092022.", region, ".rds"))  %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+  summarise(DENSITY = sum(!!sym(paste0(var, ".site"))), .groups = "drop") %>% 
+  subset(SPECIES == "APVI")
+
+tow = readRDS(paste0("data/tow.segment.", var, ".size.20002017.", region, ".rds"))  %>% 
+  subset(CENTROIDLON != 0) %>%
+  mutate(LONGITUDE = CENTROIDLON,
+         LATITUDE = CENTROIDLAT) %>% 
+  group_by(ISLAND, DEPTH, METHOD, DATE_, LATITUDE, LONGITUDE, SPECIES) %>% 
+  summarise(DENSITY = sum(!!sym(paste0(var, ".segment"))), .groups = "drop") %>% 
+  subset(SPECIES == "APVI") %>%
+  mutate(DENSITY = (DENSITY / calibr_tow))
+
+df = rbind(spc, belt, tow)
+
+library(lubridate)
+df$YEAR <- year(df$DATE_)
+df$MONTH <- month(df$DATE_)
+
+library(colorRamps)
+
+df %>% 
+  mutate(DENSITY = DENSITY*100,
+         LONGITUDE = round(LONGITUDE, 1), 
+         LATITUDE= round(LATITUDE, 1)) %>% 
+  group_by(METHOD, LONGITUDE, LATITUDE) %>%
+  summarise(DENSITY = mean(DENSITY)) %>%
+  ggplot(aes(LONGITUDE, LATITUDE)) + 
+  geom_point(aes(size = DENSITY, fill = DENSITY, color = DENSITY), shape = 21, alpha = 0.7) +
+  scale_color_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  scale_fill_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  # facet_grid(METHOD~YEAR) +
+  facet_grid(~METHOD) +
+  labs(x = expression(paste("Longitude ", degree, "W", sep = "")),
+       y = expression(paste("Latitude ", degree, "N", sep = ""))) +
+  guides(color = guide_legend(expression("Individuals per 100" ~ m^2~"")), 
+         fill = guide_legend(expression("Individuals per 100" ~ m^2~"")),
+         size = guide_legend(expression("Individuals per 100" ~ m^2~""))) + 
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20", linewidth = 0.1)) 
+
+df %>% 
+  mutate(DENSITY = DENSITY*100,
+         LONGITUDE = round(LONGITUDE, 1), 
+         LATITUDE= round(LATITUDE, 1)) %>% 
+  group_by(METHOD, LONGITUDE, LATITUDE, YEAR) %>%
+  summarise(DENSITY = mean(DENSITY)) %>%
+  ggplot(aes(LONGITUDE, LATITUDE)) + 
+  geom_point(aes(size = DENSITY, fill = DENSITY, color = DENSITY), shape = 21, alpha = 0.7) +
+  scale_color_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  scale_fill_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  facet_grid(METHOD~YEAR) +
+  labs(x = expression(paste("Longitude ", degree, "W", sep = "")),
+       y = expression(paste("Latitude ", degree, "N", sep = ""))) +
+  guides(color = guide_legend(expression("Individuals per 100" ~ m^2~"")), 
+         fill = guide_legend(expression("Individuals per 100" ~ m^2~"")),
+         size = guide_legend(expression("Individuals per 100" ~ m^2~""))) + 
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20", linewidth = 0.1)) 
+
+df %>% 
+  mutate(DENSITY = DENSITY*100) %>% 
+  ggplot(aes(DEPTH, DENSITY)) + 
+  geom_point(aes(size = DENSITY, fill = DENSITY, color = DENSITY), shape = 21, alpha = 0.7) +
+  scale_color_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  scale_fill_gradientn(colours = matlab.like(100), guide = "legend", trans = "sqrt") +
+  facet_grid(~METHOD) +
+  guides(color = guide_legend(expression("Individuals per 100" ~ m^2~"")), 
+         fill = guide_legend(expression("Individuals per 100" ~ m^2~"")),
+         size = guide_legend(expression("Individuals per 100" ~ m^2~""))) + 
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20")) 
+
+df %>%
+  mutate(YEAR = format(DATE_, "%Y")) %>%
+  group_by(YEAR, METHOD) %>%
+  summarise(mean_density = mean(DENSITY), se_density = sd(DENSITY)/sqrt(n())) %>%
+  mutate(mean_density = ifelse(mean_density == 0, NA, mean_density),
+         se_density = ifelse(se_density == 0, NA, se_density)) %>% 
+  ggplot(aes(x = YEAR, y = mean_density, color = METHOD, group = METHOD)) +
+  geom_errorbar(aes(ymin = mean_density - se_density, ymax = mean_density + se_density), position=position_dodge(width = 0.5), width = 0) +
+  geom_point(size = 3, position=position_dodge(width=0.5)) +
+  labs(x = "Year", y = "Density") +
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray10"),
+        panel.grid = element_line(color = "gray20")) 
