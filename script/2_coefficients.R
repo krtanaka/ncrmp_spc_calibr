d618c33a47a30713763867e7d4509f442c7c1dd6
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-11-10 11:16:15 -1000

    Update 2_coefficients.R

--- script/2_coefficients.R
+++ script/2_coefficients.R
@@ -12,20 +12,24 @@ rm(list = ls())
 # GCF.PRES: Gear calibration factor for presence-absence data.
 
 # Convert probability of observation of secondary method following this equation: 
-# Prob_M1=inv.logit(logit(Prob_M2)-GCF.PRES))
+# Prob_M1 = inv.logit(logit(Prob_M2) - GCF.PRES))
 
 # GCF.PRES_2.5: Lower bound of 95% probability interval of GCF.PRES.
-# GCF.PRES_95: Upper bound of 95% probability interval of GCF.PRES.
+# GCF.PRES_97.5: Upper bound of 95% probability interval of GCF.PRES.
 # GCF.POS: Gear calibration factor for positive-only data. 
 
 # Convert abundance metric of positive-only data for secondary method following this equation: 
-# Abund_M1=Abund_M2/GCF.POS
+# Abund_M1 = Abund_M2 / GCF.POS
+
 # GCF.POS_2.5: Lower bound of 95% probability interval of GCF.PRES.
-# GCF.POS_95: Upper bound of 95% probability interval of GCF.PRES.
+# GCF.POS_97.5: Upper bound of 95% probability interval of GCF.PRES.
+
 # PRES: Probability of observing a specific species for each method in the provided dataset.
 # PRES.CAL: Calibrated probability of observing a species by method (used as a check on the success of the standardization procedure).
+
 # POS: Abundance of a specific species for each method.
 # POS.CAL: Calibrated abundance of a specific species for each method (used as a check on the success of the standardization procedure).
+
 # OPUE: Observation Per Unit Effort obtained by multiplying prob. of sighting and abundance (PRES x POS) in provided data set.
 # OPUE.CAL: Observation Per Unit Effort obtained by multiplying calibrated prob. of sighting and abundance (PRES.CAL x POS.CAL) in provided data set.
 
